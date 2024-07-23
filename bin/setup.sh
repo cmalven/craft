c1e27fd69a5a336554134174cce2ac846ac2a9b0
cmalven <chris@malven.co>
2024-07-23 15:07:35 -0500

    Update setup script to handle more project setup.

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,3 +8,26 @@ grep -rl your-project-title . --exclude-dir={.git,bin,node_modules,vendor} | xar
 
 echo "Replacing project description..."
 grep -rl your-project-description . --exclude-dir={.git,bin,node_modules,vendor} | xargs sed -i '' -e "s/your-project-description/$3/g"
+
+echo "Renaming IDEA config file..."
+mv ./.idea/your-project-slug.iml ./.idea/$1.iml
+
+if [ -d ".git" ]; then
+  echo "Deleting .git directory..."
+  rm -rf .git
+fi
+
+if [ -d "node_modules" ]; then
+  echo "Deleting node_modules directory..."
+  rm -rf node_modules
+fi
+
+if [ -d "vendor" ]; then
+  echo "Deleting vendor directory..."
+  rm -rf vendor
+fi
+
+if [ -d "bin" ]; then
+  echo "Deleting bin setup scripts..."
+  rm -rf bin
+fi
