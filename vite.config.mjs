4e8db0777e44d640c6092a9862523e1f8458cf8b
cmalven <chris@malven.co>
2025-09-26 17:16:10 -0500

    Silence Sass mixed declaration warnings in Vite config.

--- vite.config.mjs
+++ vite.config.mjs
@@ -39,6 +39,7 @@ export default defineConfig(({ command, mode }) => {
       preprocessorOptions: {
         scss: {
           api: 'modern',
+          silenceDeprecations: ['mixed-decls'],
         },
       },
     },
